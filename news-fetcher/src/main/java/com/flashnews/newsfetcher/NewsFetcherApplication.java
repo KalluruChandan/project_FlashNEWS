5029d3721f4846833e9fc2d809a2157509ef918a
chandan <chandankalluri9@gmail.com>
2024-05-08 11:38:44 +0530

    Initial Commit

---
+++ news-fetcher/src/main/java/com/flashnews/newsfetcher/NewsFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.flashnews.newsfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsFetcherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsFetcherApplication.class, args);
+	}
+
+}
